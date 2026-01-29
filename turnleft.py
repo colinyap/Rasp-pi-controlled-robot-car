54afd664b57bc10bc1daaa249d57190b3a950d13
Colin <colinyap636@gmail.com>
2026-01-30 00:59:24 +0800

    Deleted redundant file

--- turnleft.py
+++
@@ -1,8 +0,0 @@
-# Function to turn the robot left
-# angle: angle to turn (degrees, based on calibration)
-def turnleft(angle):
-    GPIO.output(IN1, True)   # Left motor forward
-    GPIO.output(IN2, False)
-    GPIO.output(IN3, False)  # Right motor backward
-    GPIO.output(IN4, True)
-    time.sleep(angle / Duty_cycle_angle)
